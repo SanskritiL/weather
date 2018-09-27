659025e9cf4a664677b65513fba5ed4ac8844768
slamsal <slamsal@go.olemiss.edu>
2018-09-27 02:16:06 -0500

    Name and Icon is working.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { WeatherProvider } from '../../providers/weather/weather';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  weather:any;
+  icon:string;
+  location:{
+    city:string
+  }
+  constructor(public navCtrl: NavController,private weatherProvider:WeatherProvider) {
+     
+  }
+  ionViewWillEnter(){
+    this.location = {
+      city: 'London'
+    }
+    this.weatherProvider.getWeather(this.location.city)
+    .subscribe(weather =>{
+       this.weather = weather;
+      var temp = this.weather.main.temp;
+      var icon =this.weather.weather[0].icon;
+       console.log(icon);
+    }
+      );
+      
   }
 
+  
 }
---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+/*
+  Generated class for the WeatherProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class WeatherProvider {
+
+ city;
+ apikey='25299114f3b698c64cd83719c107cffc';
+ url;
+  constructor(public http: HttpClient) {
+    console.log('Hello WeatherProvider Provider');
+    this.url='http://api.openweathermap.org/data/2.5/weather?q=';
+  }
+
+getWeather(city){
+  return this.http.get(this.url+city+'&APPID='+this.apikey);
+}
+  
+  
+}
